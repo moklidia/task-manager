fbb783819adfac213da2ee66aa3b37b39a0c0be3
moklidia <kitsunegakkou@gmail.com>
2019-08-11 15:04:27 +0400

    add assoc and validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
   validates :name, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,15 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+<<<<<<< HEAD
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, format: { with: /.*@.*/ }, uniqueness: true
+=======
+  has_many :assigned_tasks, class_name: 'Task', foreing_key: :assignee_id
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, format: { with: /^.*@.*$/ }, uniqueness: true
+>>>>>>> add assoc and validation
 end
