4c87606cd53c8b22432b4f3d4189ed5323b6d499
moklidia <kitsunegakkou@gmail.com>
2019-08-12 17:52:26 +0400

    fix lint

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,38 +1,40 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
-	belongs_to :author, class_name: 'User'
-	belongs_to :assignee, class_name: 'User', optional: true
-	validates :name, presence: true
-	validates :description, presence: true
-	validates :author, presence: true
-	validates :description, length: { maximum: 500 }
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :description, length: { maximum: 500 }
 
-	state_machine :initial => :new_task do
-		event :start do
-			transition :new_task => :in_development
-		end
+  state_machine initial: :new_task do
+    event :start do
+      transition new_task: :in_development
+    end
 
-		event :archive do
-			transition [:new_task, :ready_for_release] => :archived
-		end
+    event :archive do
+      transition %i[new_task ready_for_release] => :archived
+    end
 
-		event :submit do
-			transition :in_development => :in_qa
-		end
+    event :submit do
+      transition in_development: :in_qa
+    end
 
-		event :review do
-			transition :in_qa => :in_code_review
-		end
+    event :review do
+      transition in_qa: :in_code_review
+    end
 
-		event :improve do
-			transition :in_code_review => :in_development
-		end
+    event :improve do
+      transition in_code_review: :in_development
+    end
 
-		event :prepare do
-			transition :in_qa => :ready_for_release
-		end
-		
-		event :release do
-			transition :ready_for_release => :released
-		end
-	end
+    event :prepare do
+      transition in_qa: :ready_for_release
+    end
+
+    event :release do
+      transition ready_for_release: :released
+    end
+  end
 end
