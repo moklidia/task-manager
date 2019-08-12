a582fe4788ed2d6448afc50ce06d9dba494f3121
moklidia <kitsunegakkou@gmail.com>
2019-08-12 10:51:48 +0400

    add state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,34 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+  
+	state_machine :initial => :new_task do
+		event :start do
+			transition :new_task => :in_development
+		end
+
+		event :archive do
+			transition [:new_task, :ready_for_release] => :archived
+		end
+
+		event :submit do
+			transition :in_development => :in_qa
+		end
+
+		event :review do
+			transition :in_qa => :in_code_review
+		end
+
+		event :improve do
+			transition :in_code_review => :in_development
+		end
+
+		event :prepare do
+			transition :in_qa => :ready_for_release
+		end
+		
+		event :release do
+			transition :ready_for_release => :released
+		end
+	end
 end
