a7eec17a5d965cf4d5f4f363153cab96606c62d7
moklidia <kitsunegakkou@gmail.com>
2019-08-12 17:52:26 +0400

    fix lint

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,34 +8,34 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
-  
-	state_machine :initial => :new_task do
-		event :start do
-			transition :new_task => :in_development
-		end
 
-		event :archive do
-			transition [:new_task, :ready_for_release] => :archived
-		end
+  state_machine initial: :new_task do
+    event :start do
+      transition new_task: :in_development
+    end
 
-		event :submit do
-			transition :in_development => :in_qa
-		end
+    event :archive do
+      transition %i[new_task ready_for_release] => :archived
+    end
 
-		event :review do
-			transition :in_qa => :in_code_review
-		end
+    event :submit do
+      transition in_development: :in_qa
+    end
 
-		event :improve do
-			transition :in_code_review => :in_development
-		end
+    event :review do
+      transition in_qa: :in_code_review
+    end
 
-		event :prepare do
-			transition :in_qa => :ready_for_release
-		end
-		
-		event :release do
-			transition :ready_for_release => :released
-		end
-	end
+    event :improve do
+      transition in_code_review: :in_development
+    end
+
+    event :prepare do
+      transition in_qa: :ready_for_release
+    end
+
+    event :release do
+      transition ready_for_release: :released
+    end
+  end
 end
