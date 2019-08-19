248953f0ea3c10bec7311704055b39191d389301
moklidia <kitsunegakkou@gmail.com>
2019-08-19 10:37:16 +0400

    update formatter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 require 'simplecov'
 require 'coveralls'
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start 'rails'
+Coveralls.wear!
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
+SimpleCov.start
 
 # frozen_string_literal: true
 
