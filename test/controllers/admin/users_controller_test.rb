2cd5b9e6843109ce2b41447d0f0067890ea79b36
moklidia <kitsunegakkou@gmail.com>
2019-08-22 16:37:49 +0400

    add admin_users index

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -7,9 +7,15 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     sign_in_as admin
   end
 
+  test "should get index" do
+  	get admin_users_url
+  	assert_response :success
+  end
+
   test "should get show" do
     user = create(:user)
     get admin_user_url(user)
     assert_response :success
   end
+
 end
