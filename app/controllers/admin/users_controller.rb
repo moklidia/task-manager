03b3a8b931c6273c3efa806420bfe643891dfcfb
moklidia <kitsunegakkou@gmail.com>
2019-08-22 23:14:02 +0400

    fix form partial

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,43 +1,43 @@
 class Admin::UsersController < Admin::ApplicationController
   def index
-  	@q = User.ransack(params[:q])
-  	@users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
+    @q = User.ransack(params[:q])
+    @users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
   end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def edit
-  	@user = User.find(params[:id])
-  end
-
-  def create
-  	@user = User.new(user_params)
-  	if @user.save
-  	  redirect_to admin_users_url
-  	else
-  	  render :new_admin_user_url
-  	end
+    @user = User.find(params[:id])
   end
 
   def update
-  	@user = User.find(params[:id])
-  	if @user.update(user_params)
-  	  redirect_to admin_users_url
-  	else
-  	  render :edit
-  	end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_url
+    else
+      render :edit
+    end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
     redirect_to admin_users_url
+  end
+
+  def create
+    @user = User.new(user_params)
+     if @user.save
+      redirect_to admin_users_url
+    else
+      render :new_admin_user_url
+    end
   end
 
   private
