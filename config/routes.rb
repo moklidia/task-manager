217af9967e46a994549e7edce19248a6b462b083
moklidia <kitsunegakkou@gmail.com>
2019-08-25 11:16:21 +0400

    add api v1

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :tasks, only: %i[index show create update destroy]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
