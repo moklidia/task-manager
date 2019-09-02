4d49f979d0e23a924cb56134a45c383b9ed245fe
moklidia <kitsunegakkou@gmail.com>
2019-09-02 12:27:42 +0400

    add trello board

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,7 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import WebpackerReact from 'webpacker-react'
+import TaskBoard from './components/TaskBoard'
+
+WebpackerReact.setup({TaskBoard})
