e6bd4812c3ef6a210dd69b7d8016adc32570088a
moklidia <kitsunegakkou@gmail.com>
2019-09-02 12:30:32 +0400

    fix lint

--- config/environment.rb
+++ config/environment.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
-require File.expand_path('../application', __FILE__)
-require File.expand_path('../rollbar', __FILE__)
+require File.expand_path('application', __dir__)
+require File.expand_path('rollbar', __dir__)
 
-notify = ->(e) do
-  begin
-    Rollbar.with_config(use_async: false) do
-      Rollbar.error(e)
-    end
-  rescue
-    Rails.logger.error "Synchronous Rollbar notification failed.  Sending async to preserve info"
+notify = lambda do |e|
+  Rollbar.with_config(use_async: false) do
     Rollbar.error(e)
   end
+rescue StandardError
+  Rails.logger.error 'Synchronous Rollbar notification failed.  Sending async to preserve info'
+  Rollbar.error(e)
 end
 
 begin
   Rails.application.initialize!
 rescue Exception => e
-  notify.(e)
+  notify.call(e)
   raise
 end
