2a0f4677cd65e507f34d6b2c59e8589a7911f1ee
moklidia <kitsunegakkou@gmail.com>
2019-09-05 17:33:00 +0400

    change email to pass validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@localhost')
+admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@localhost.ru')
 admin.password = 'admin'
 admin.save
 
