88fb4334ee1f060fc887872a70a95fa9bf96542f
moklidia <kitsunegakkou@gmail.com>
2019-09-05 22:20:44 +0400

    implemented task editing

--- app/javascript/packs/components/TaskBoard.js
+++ app/javascript/packs/components/TaskBoard.js
@@ -4,6 +4,7 @@ import { fetch } from './Fetch';
 import LaneHeader from './LaneHeader';
 import Button from 'react-bootstrap/Button';
 import AddPopup from './AddPopup';
+import EditPopup from './EditPopup';
 
 export default class TasksBoard extends React.Component {
   state = {
@@ -16,7 +17,9 @@ export default class TasksBoard extends React.Component {
       released: null,
       archived: null
     },
-    addPopupShow: false
+    addPopupShow: false,
+    editPopupShow: false,
+    editCardId: null
   }
 
   generateLane(id, title) {
@@ -109,6 +112,32 @@ export default class TasksBoard extends React.Component {
     });
 }
 
+onCardClick = (cardId) => {
+  this.setState({editCardId: cardId});
+  this.handleEditShow();
+}
+
+handleEditClose = ( edited = '' ) => {
+  this.setState({ editPopupShow: false, editCardId: null});
+  switch (edited) {
+    case 'new_task':
+    case 'in_development':
+    case 'in_qa':
+    case 'in_code_review':
+    case 'ready_for_release':
+    case 'released':
+    case 'archived':
+      this.loadLine(edited);
+      break;
+    default:
+      break;
+  }
+}
+
+handleEditShow = () => {
+  this.setState({ editPopupShow: true });
+}
+
   render() {
     return <div>
       <h1>Your tasks</h1>
@@ -121,10 +150,16 @@ export default class TasksBoard extends React.Component {
         draggable
         laneDraggable={false}
         handleDragEnd={this.handleDragEnd}
+        onCardClick={this.onCardClick}
       />
       <AddPopup
         show = {this.state.addPopupShow}
         onClose={this.handleAddClose}
+      />
+      <EditPopup
+        show = {this.state.editPopupShow}
+        onClose={this.handleEditClose}
+        cardId ={this.state.editCardId}
       />
     </div>;
   }
